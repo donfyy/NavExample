4c28f4765ed3d2db3f25cd443ab1a746fd8958ee
donfyy <xinxin.wang@pintec.com>
2020-04-26 18:33:11 +0800

    add more

---
+++ app/src/main/java/com/example/navexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.navexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.navexample.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
